b266045bf79a3fe03cb89b74ac702a83a1793bd5
FeliciaMarlove <missflorencemary@hotmail.com>
2021-01-30 10:11:51 +0100

    User Console Interface - Catching input exception in getIntegerInput

--- src/main/java/be/fmary/Ihm/UserConsoleInterface.java
+++ src/main/java/be/fmary/Ihm/UserConsoleInterface.java
@@ -3,6 +3,7 @@ package main.java.be.fmary.Ihm;
 import main.java.be.fmary.facts.Fact;
 import main.java.be.fmary.rules.Rule;
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -15,7 +16,15 @@ public class UserConsoleInterface {
 
     public int getIntegerInput(String question) {
         System.out.println(question);
-        return sc.nextInt();
+        int input;
+        try {
+            input = sc.nextInt();
+            return input;
+        } catch (InputMismatchException e) {
+            System.out.println("Please only input an integer value");
+            getIntegerInput(question);
+        }
+        return -999_999;
     }
 
     public boolean getBooleanInput(String question) {
